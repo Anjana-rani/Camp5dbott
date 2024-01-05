f75de9a7f983df6018b92b3e25ed9ded269518f5
Anjana-rani <anjanarani0099@gmail.com>
2024-01-05 17:29:19 +0530

    upload

---
+++ ottproject/ottapp/admin.py
@@ -0,0 +1,14 @@
+
+# Register your models here.
+from django.contrib import admin
+from .models import Customer
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('first_name','username', 'password', 'email','phone_number')  # Customize the fields you want to display
+    # Add other configurations as needed
+
+
+
+
+
