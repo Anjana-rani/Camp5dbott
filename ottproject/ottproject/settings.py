6655802b09d5cdc49d2fc7e654893697c8f6f095
balachandrans195 <bala1022389@gmail.com>
2024-01-06 10:21:14 +0530

    pro

--- ottproject/ottproject/settings.py
+++ ottproject/ottproject/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'ottadbapp',
         'USER' : 'root',
-        'PASSWORD' : 'faith',
+        'PASSWORD' : 'bala123',
         'HOST' : 'localhost',
         'PORT': 3306,
     }
